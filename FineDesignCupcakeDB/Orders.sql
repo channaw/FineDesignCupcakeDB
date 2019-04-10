232df725d40b6bb48a5a6b62c9ea9a931353f1ef
Channa Wijesinghe <manzyron@hotmail.com>
2019-04-10 17:42:09 -0400

    initial check in, added orders table

---
+++ FineDesignCupcakeDB/Orders.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Orders]
+(
+	[OrdersId] INT NOT NULL PRIMARY KEY,
+	[Notes] nvarchar(255),
+	[Quantity] int,
+	[OrderDate] Datetime,
+	[DeliveryDate] Datetime,
+	[Status] tinyint
+)
+
