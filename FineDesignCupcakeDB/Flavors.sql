f1d8ca41300a738fd55c175e65317af93acb7a1a
Channa Wijesinghe <manzyron@hotmail.com>
2019-04-12 15:50:09 -0400

    added image and thumbnail to flavors table

--- FineDesignCupcakeDB/Flavors.sql
+++ FineDesignCupcakeDB/Flavors.sql
@@ -3,6 +3,8 @@
 	[FlavorId] INT NOT NULL PRIMARY KEY,
 	[FlavorName] nvarchar(50), 
     [CreatedDate] DATETIME NULL, 
-    [ModifiedDate] DATETIME NULL,
+    [ModifiedDate] DATETIME NULL, 
+    [Image] NVARCHAR(255) NULL, 
+    [Thumbnail] NVARCHAR(255) NULL,
 	
 )
